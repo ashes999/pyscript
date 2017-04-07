6a1b22eb3cef92851ee4d657d8ba60b6c15f8d35
ashes999 <ashes999@yahoo.com>
2017-04-07 16:06:57 -0400

    Convert parseExpression into Haxe

--- src/pyscript/EggEngine.hx
+++ src/pyscript/EggEngine.hx
@@ -9,18 +9,73 @@ class EggEngine
 
     public function execute(script:String):Dynamic
     {
-        return 1;
+        return new EggParser().parseExpression(script);
     }
+}
 
-    // Interact with a native Haxe object
-    public function addHostObject(obj:Dynamic):Void
+class EggParser
+{
+    public function new() { }
+    
+    public function parseExpression(program:String)
     {
+        program = skipSpace(program);
+        var expr:TypeAndValue;
+        var matchedLength:Int;
 
+        var valueRegex = new EReg("^\"([^\"]*)\"", "");
+        var numberRegex = new EReg("^(\\d+)\\b", "");
+        var wordRegex = new EReg("^[^\\s(),\"]+", "");
+
+        if (valueRegex.match(program))
+        {
+            expr = new TypeAndValue("value", valueRegex.matched(1));
+            matchedLength = valueRegex.matched(0).length;
+        }
+        else if (numberRegex.match(program))
+        {
+            expr = new TypeAndValue("value", Std.parseInt(numberRegex.matched(1)));
+            matchedLength = numberRegex.matched(0).length;
+        }
+        else if (wordRegex.match(program))
+        {
+            expr = new TypeAndValue("word", wordRegex.matched(0));
+            matchedLength = wordRegex.matched(0).length;
+        }
+        else
+        {
+            throw "Unexpected syntax: " + program;
+        }
+
+        //return parseApply(expr, program.substr(matchedLength));
     }
 
-    // Allow us to instantiate a native Haxe class
-    public function addHostType<T>(clazz:Class<T>):Void
+    private function skipSpace(text:String):String
     {
+        // NO TABS ALLOWED. Only spaces.
+        var first = text.indexOf(" ");
+        
+        if (first == -1) {
+            return "";
+        }
 
+        return text.substr(first);
     }
+}
+
+class TypeAndValue
+{
+    public var type(default, null):String;
+    public var value(default, null):Dynamic;
+
+    public function new(type:String, value:Dynamic)
+    {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+class Program : TypeAndValue
+{
+
 }
